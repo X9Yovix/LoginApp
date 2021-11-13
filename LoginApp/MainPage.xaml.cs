01913e79d114c367596759e0b055914f3c7b700d
Karim Ouazzou <karimouazzou@gmail.com>
2021-11-13 15:41:29 +0100

    Add project files.

---
+++ LoginApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void DisplayMessage(Object sender, EventArgs e)
+        {
+            lblMessage.Text = "Welcome " + textName.Text;
+            lblMessage.TextColor = Color.White; 
+            lblMessage.BackgroundColor = Color.Orange;
+        }
+    }
+}
